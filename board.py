36f4251f07096d6e48eac0ffe6445a1b02e248f2
Kelvin Chen <kelvin.chen.nc@gmail.com>
2021-12-09 18:45:27 -0500

    Board printing functionality added

---
+++ board.py
@@ -0,0 +1,11 @@
+row1 = ['-', '-', '-']
+row2 = ['-', '-', '-']
+row3 = ['-', '-', '-']
+board = [row1, row2, row3]
+
+
+def printboard(board):
+    for i in board:
+        print(i)
+
+printboard(board)
